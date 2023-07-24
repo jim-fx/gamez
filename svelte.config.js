6352eed79ffd683f71ee47b50e8a648deba1b950
Max Richter <jim-x@web.de>
2023-07-24 16:32:58 +0200

    feat: fix ci

--- svelte.config.js
+++ svelte.config.js
@@ -1,8 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/kit/vite';
 
-const { BASE_PATH } = process.env;
-
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,9 +9,6 @@ const config = {
 
   // dev: true,
   kit: {
-    paths: {
-      base: BASE_PATH
-    },
     alias: {
       $i18n: 'src/i18n',
     },
